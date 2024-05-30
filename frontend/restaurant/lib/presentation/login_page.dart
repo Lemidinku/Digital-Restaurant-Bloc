8fb43144df102feeb20705f73ad0fdcfd83de4eb
Eba Adisu <ebaadisu2@gmail.com>
2024-05-30 18:57:32 +0300

    working tests

--- frontend/restaurant/lib/presentation/login_page.dart
+++ frontend/restaurant/lib/presentation/login_page.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'widget/login-form.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: LoginPage(),
+//     );
+//   }
+// }
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
--- frontend/restaurant/test/widget_test/#not_done.dart
+++ frontend/restaurant/test/widget_test/#not_done.dart
@@ -1,1 +1,4 @@
 // detail_test.dart
+// bottom_nav_test.dart
+// login_page_test.dart
+// plus_minus_input_test.dart
