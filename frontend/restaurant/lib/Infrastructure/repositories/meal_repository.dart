ea8571e1fd0faed97d69b2d8c8efabeca4f9fe17
GedionAfework <gedionmekbeb2@gmail.com>
2024-05-31 13:24:34 +0300

    meal_repository test update

--- frontend/restaurant/lib/Infrastructure/repositories/meal_repository.dart
+++ frontend/restaurant/lib/Infrastructure/repositories/meal_repository.dart
@@ -91,11 +91,3 @@ class MealRepository {
     }
   }
 }
-
-// meal repository test
-
-void main() {
-  String baseUrl = 'http://10.0.2.2:9000';
-  MealRepository mealRepository = MealRepository(baseUrl: baseUrl);
-  print(mealRepository.fetchMeals());
-}
